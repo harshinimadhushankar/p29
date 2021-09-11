5f34bff2bd69ba42e4946e1e75ba68adbec31e20
harshinimadhushankar <83548524+harshinimadhushankar@users.noreply.github.com>
2021-09-11 20:37:44 +0100

    Add files via upload

---
+++ js/SlingShot.js
@@ -0,0 +1,38 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        this.pointB=pointB;
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA =null;
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            stroke(255,0,0);
+            if(pointA.x < 220){
+            strokeWeight(4);
+            line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x-20, pointA.y, pointB.x+30, pointB.y);
+            }
+
+            
+           // write stroke() to set the color to red
+
+           // Write line() to draw a line from pointA to pointB
+
+        }
+    }
+    
+}
